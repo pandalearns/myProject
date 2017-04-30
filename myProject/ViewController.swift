19ba3e56468d9e845f498f2a938a1d02ed78dbe6
Ranz Barron <ranzbarron@ranzs-macbook-pro.local>
2017-04-30 14:24:52 +0800

    Initial Commit

---
+++ myProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myProject
+//
+//  Created by Ranz Barron on 30/04/2017.
+//  Copyright © 2017 Ranz Barron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
